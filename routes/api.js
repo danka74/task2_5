c1554cea7d9ab05d36781000e26936e4732b2e95
danka74 <daniel.karlsson@liu.se>
2015-05-18 17:08:19 +0200

    added some API routes

--- routes/api.js
+++ routes/api.js
@@ -1,3 +1,6 @@
+var express = require('express');
+var router = express.Router();
+
 var mongoose = require('mongoose');
 var Case = mongoose.model('Case');
 var CasePart = mongoose.model('CasePart');
@@ -18,3 +21,21 @@ router.post('/cases', function(req, res, next) {
     res.json(_case);
   });
 });
+
+router.param('case', function(req, res, next, id) {
+  var query = Case.findById(id);
+
+  query.exec(function (err, _case){
+    if (err) { return next(err); }
+    if (!_case) { return next(new Error('can\'t find case')); }
+
+    req.case = _case;
+    return next();
+  });
+});
+
+router.get('/cases/:case', function(req, res) {
+  res.json(req.case);
+});
+
+module.exports = router;
