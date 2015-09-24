85363cc27674c6e6aaa28bdace768018424d0019
danka74 <daniel.karlsson@liu.se>
2015-09-24 17:08:38 +0200

    Changed default date to function!

--- models/CaseBinding.js
+++ models/CaseBinding.js
@@ -3,19 +3,19 @@ var mongoose = require('mongoose');
 var CaseBindingSchema = new mongoose.Schema({
   template: {type: mongoose.Schema.Types.ObjectId, ref: 'CaseTemplate'},
   scenario: {type: Boolean },
-  date: {type: Date, default: Date.now() },
+  date: {type: Date, default: Date.now },
   lhsBinding: {
 	  codeSystem: { type: String },
 	  target: { type: String },
 	  assessment: { type: String },
 	  source: {type: String },
-	  comments: [ {text: String, date: {type: Date, default: Date.now() }} ]
+	  comments: [ {text: String, date: {type: Date, default: Date.now }} ]
   },
   rhsOverall: {
 	  codeSystem: { type: String },
 	  target: { type: String },
 	  assessment: { type: String },
-	  comments: [ {text: String, date: {type: Date, default: Date.now() }} ]
+	  comments: [ {text: String, date: {type: Date, default: Date.now }} ]
   },
   rhsBindings: [ {
 	  codeSystem: { type: String },
@@ -23,9 +23,9 @@ var CaseBindingSchema = new mongoose.Schema({
 	  assessment: { type: String },
 	  source: {type: String },
 	  sourceTemplatePart: mongoose.Schema.Types.ObjectId ,
-	  comments: [ {text: String, date: {type: Date, default: Date.now() }} ]
+	  comments: [ {text: String, date: {type: Date, default: Date.now }} ]
   } ],
-  comments: [ {text: String, date: {type: Date, default: Date.now() }} ],
+  comments: [ {text: String, date: {type: Date, default: Date.now }} ],
   user: { uid: {type: String, index: true} }
 });
 
