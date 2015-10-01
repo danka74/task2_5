a5082ed474273c9bacf14b80c559900190f5b6f7
danka74 <daniel.e.karlsson@gmail.com>
2015-10-01 19:24:51 +0200

    Added fix for IE, disabled AJAX caching

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -1,25 +1,40 @@
-angular.module(
-		'BindingApp',
-		[ 'ngRoute', 'ui.bootstrap', 'MainControllerModule',
-				'TemplateServiceModule', 'BindingServiceModule', /*'uiSwitch',*/ 'angular-jwt', 'hc.marked', 'sticky' ]).factory(
-		'authInterceptor',
-		function($rootScope, $q, $window) {
-			return {
-				request : function(config) {
-					config.headers = config.headers || {};
-					if ($window.sessionStorage.token) {
-						config.headers.Authorization = 'Bearer '
-								+ $window.sessionStorage.token;
-					}
-					return config;
-				},
-				response : function(response) {
-					if (response.status === 401) {
-						// handle the case where the user is not authenticated
-					}
-					return response || $q.when(response);
-				}
-			};
-		}).config(function($httpProvider) {
-	$httpProvider.interceptors.push('authInterceptor');
-});
+angular
+		.module(
+				'BindingApp',
+				[ 'ngRoute', 'ui.bootstrap', 'MainControllerModule',
+						'TemplateServiceModule', 'BindingServiceModule', /* 'uiSwitch', */
+						'angular-jwt', 'hc.marked', 'sticky' ])
+		.factory(
+				'authInterceptor',
+				function($rootScope, $q, $window) {
+					return {
+						request : function(config) {
+							config.headers = config.headers || {};
+							if ($window.sessionStorage.token) {
+								config.headers.Authorization = 'Bearer '
+										+ $window.sessionStorage.token;
+							}
+							return config;
+						},
+						response : function(response) {
+							if (response.status === 401) {
+								// handle the case where the user is not
+								// authenticated
+							}
+							return response || $q.when(response);
+						}
+					};
+				})
+		.config(
+				[
+						'$routeProvider',
+						'$httpProvider',
+						function($routeProvider, $httpProvider) {
+							$httpProvider.defaults.cache = false;
+							if (!$httpProvider.defaults.headers.get) {
+								$httpProvider.defaults.headers.get = {};
+							}
+							// disable IE ajax request caching
+							$httpProvider.defaults.headers.get['If-Modified-Since'] = '0';
+							$httpProvider.interceptors.push('authInterceptor');
+						} ]);
